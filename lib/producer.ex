562e05f8b7d6a6158de01a7a0184b71fb08da186
Bruno Abrantes <bruno@brunoabrantes.com>
2015-01-13 16:58:06 +0100

    Supports RabbitMQ configuration via config.exs

--- lib/producer.ex
+++ lib/producer.ex
@@ -2,7 +2,7 @@ defmodule Microbrew.Producer do
   import AMQP
 
   def publish(exchange, payload) do
-    {:ok, conn} = AMQP.Connection.open("amqp://guest:guest@localhost")
+    {:ok, conn} = AMQP.Connection.open Microbrew.Config.rabbitmq_url
     {:ok, chan} = AMQP.Channel.open(conn)
 
     AMQP.Basic.publish chan, exchange, "", payload
