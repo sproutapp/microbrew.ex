e7ddd30bbfeb951049924408bdb9c56350ad4182
Bruno Abrantes <inf0rmer.realm@gmail.com>
2015-02-12 23:01:42 +0100

    Improves tests with the newer version of Pavlov

--- test/agent_test.exs
+++ test/agent_test.exs
@@ -5,7 +5,7 @@ defmodule AgentTest do
 
   import Microbrew.Agent
 
-  describe ".new" do
+  describe "Agent" do
     let :consumer do
       %Microbrew.Consumer{channel: nil, queue: nil}
     end
@@ -24,158 +24,150 @@ defmodule AgentTest do
       :ok
     end
 
-    it "returns an %Agent with the correct properties" do
-      expect sample.__struct__
-        |> to_eq Microbrew.Agent
+    describe ".new" do
+      it "returns an %Agent with the correct properties" do
+        expect sample.__struct__
+          |> to_eq Microbrew.Agent
 
-      expect sample.exchange
-        |> to_eq "exchange"
+        expect sample.exchange
+          |> to_eq "exchange"
 
-      expect sample.queue
-        |> to_eq "queue"
+        expect sample.queue
+          |> to_eq "queue"
 
-      expect sample.queue_error
-        |> to_eq "queue_error"
-    end
-
-    it "creates a Consumer" do
-      sample = Microbrew.Agent.new(
-        exchange:    "exchange",
-        queue:       "queue",
-        queue_error: "queue_error"
-      )
-
-      expect sample.consumer
-        |> to_eq consumer
-    end
-  end
-
-  describe ".consume" do
-    let :consumer do
-      %Microbrew.Consumer{channel: nil, queue: nil}
-    end
-
-    let :sample do
-      %Microbrew.Agent{
-        exchange:    "exchange",
-        queue:       "queue",
-        queue_error: "queue_error"
-      }
-    end
-
-    before :each do
-      allow(Microbrew.Consumer)
-        |> to_receive(new: fn (_, _, _) -> {:ok, consumer} end)
-      :ok
-    end
-
-    it "creates a Consumer" do
-      sample = sample |> consume
-
-      expect Microbrew.Consumer
-        |> to_have_received :new
-        |> with ["exchange", "queue", "queue_error"]
-
-      expect sample.consumer
-        |> to_eq consumer
-    end
-
-    context "When there is already a Consumer with a non-nil channel" do
-      let :a_consumer do
-        %Microbrew.Consumer{channel: "channel", queue: nil}
+        expect sample.queue_error
+          |> to_eq "queue_error"
       end
 
+      it "creates a Consumer" do
+        sample = Microbrew.Agent.new(
+          exchange:    "exchange",
+          queue:       "queue",
+          queue_error: "queue_error"
+        )
+
+        expect sample.consumer
+          |> to_eq consumer
+      end
+    end
+
+    describe ".consume" do
       before :each do
-        allow(Microbrew.Consumer, [:no_link, :passthrough])
-          |> to_receive(new: fn (_, _, _) -> {:ok, a_consumer} end)
-
         allow(Microbrew.Agent, [:no_link, :passthrough])
-          |> to_receive(stop: fn(a) -> a end)
-
+        allow(Microbrew.Consumer)
+          |> to_receive(new: fn (_, _, _) -> {:ok, consumer} end)
         :ok
       end
 
-      it "stops the agent" do
-        agent = %Microbrew.Agent{
-          exchange:    "exchange",
-          queue:       "queue",
-          queue_error: "queue_error",
-          consumer:    a_consumer
-        }
+      it "creates a Consumer" do
+        sample = sample |> consume
 
-        agent |> consume
+        expect Microbrew.Consumer
+          |> to_have_received :new
+          |> with ["exchange", "queue", "queue_error"]
 
-        expect Microbrew.Agent
-          |> to_have_received :stop
-          |> with %{agent | consumer: a_consumer}
+        expect sample.consumer
+          |> to_eq consumer
+      end
+
+      context "When there is already a Consumer with a non-nil channel" do
+        let :consumer do
+          %Microbrew.Consumer{channel: "channel", queue: nil}
+        end
+
+        let :agent do
+          %Microbrew.Agent{
+            exchange:    "exchange",
+            queue:       "queue",
+            queue_error: "queue_error",
+            consumer:    consumer
+          }
+        end
+
+        before :each do
+          allow(Microbrew.Consumer, [:no_link, :passthrough])
+            |> to_receive(new: fn (_, _, _) -> {:ok, consumer} end)
+
+          allow(Microbrew.Agent, [:no_link, :passthrough])
+            |> to_receive(stop: fn(a) -> a end)
+
+          :ok
+        end
+
+        it "stops the agent" do
+          agent |> consume
+
+          expect Microbrew.Agent
+            |> to_have_received :stop
+            |> with %{agent | consumer: consumer}
+        end
       end
     end
-  end
 
-  describe ".signal/2" do
-    let :agent do
-      %Microbrew.Agent{}
-    end
+    describe ".signal/2" do
+      let :agent do
+        %Microbrew.Agent{}
+      end
 
-    it "creates a signal with a given agent and event" do
-      sig = agent |> signal(:data)
+      it "creates a signal with a given agent and event" do
+        sig = agent |> signal(:data)
 
-      expect sig.agent
-        |> to_eq agent
+        expect sig.agent
+          |> to_eq agent
 
-      expect sig.event
-        |> to_eq :data
-    end
-
-    it "creates a signal with a generated correlation id" do
-      sig = agent |> signal(:data)
-
-      expect sig.cid
-        |> not_to_be_nil
-    end
-  end
-
-  describe ".signal/3" do
-    context "When the third parameter is nil" do
-      let :signal do
-        %Microbrew.Agent{} |> signal("my-event", nil)
+        expect sig.event
+          |> to_eq :data
       end
 
       it "creates a signal with a generated correlation id" do
-        expect signal.cid
+        sig = agent |> signal(:data)
+
+        expect sig.cid
           |> not_to_be_nil
       end
     end
 
-    context "When the third parameter is not nil" do
-      let :signal_with_cid do
-        %Microbrew.Agent{} |> signal("my-event", "my-id")
+    describe ".signal/3" do
+      context "When the third parameter is nil" do
+        let :signal do
+          import Microbrew.Agent
+          %Microbrew.Agent{} |> signal("my-event", nil)
+        end
+
+        it "creates a signal with a generated correlation id" do
+          expect signal.cid
+            |> not_to_be_nil
+        end
       end
 
-      it "creates a signal with a given correlation id" do
-        expect signal_with_cid.cid
-          |> to_eq "my-id"
+      context "When the third parameter is not nil" do
+        let :signal_with_cid do
+          import Microbrew.Agent
+          %Microbrew.Agent{} |> signal("my-event", "my-id")
+        end
+
+        it "creates a signal with a given correlation id" do
+          expect signal_with_cid.cid
+            |> to_eq "my-id"
+        end
       end
     end
-  end
 
-  describe ".on" do
-    context "When the event is :data" do
+    describe ".on" do
+      let :a_signal do
+        import Microbrew.Agent
+        sample |> signal("some::event")
+      end
+
+      let :callback do
+        fn (_, _) -> end
+      end
+
       before :each do
         allow(AMQP.Queue)
           |> to_receive(subscribe: fn (_, _, _) -> nil end)
 
         :ok
-      end
-
-      let :a_signal do
-        %Microbrew.Agent{
-          queue: "queue",
-          consumer: %Microbrew.Consumer{channel: "channel", queue: nil},
-        } |> signal("some::event")
-      end
-      let :callback do
-        fn (_, _) -> end
       end
 
       it "returns the Agent" do
@@ -193,15 +185,8 @@ defmodule AgentTest do
           |> with [a_signal.agent.consumer.channel, a_signal.agent.queue, :_]
       end
 
-      describe "Callback execution" do
+      context "Callback execution" do
         context "When \"payload[\"event\"]\" matches the subscribed event" do
-          let :a_signal do
-            %Microbrew.Agent{
-              queue: "queue",
-              consumer: %Microbrew.Consumer{channel: "channel", queue: nil},
-            } |> signal("some::event")
-          end
-
           it "fires the callback with the payload data" do
             allow(AMQP.Queue)
               |> to_receive(subscribe: fn (_, _, cb) ->
@@ -223,119 +208,91 @@ defmodule AgentTest do
         end
       end
     end
-  end
 
-  describe ".stream" do
-    let :a_signal do
-      %Microbrew.Agent{
-        queue: "queue",
-        consumer: %Microbrew.Consumer{channel: "channel", queue: nil},
-      } |> signal("some::event")
+    describe ".stream" do
+      let :a_signal do
+        import Microbrew.Agent
+        %Microbrew.Agent{
+          queue: "queue",
+          consumer: %Microbrew.Consumer{channel: "channel", queue: nil},
+        } |> signal("some::event")
+      end
+
+      let :data do
+        payload = %{
+          "event" => "some::event",
+          "data"  => "some data",
+          "cid"   => "my-id"
+        }
+        {_, payload} = JSX.encode(payload)
+        payload
+      end
+
+      before :each do
+        allow(AMQP.Basic)
+          |> to_receive(get: fn (_channel, _queue) ->
+            {:ok, data, []}
+          end)
+
+        :ok
+      end
+
+      it "returns a Stream of data" do
+        stream = a_signal |> stream
+
+        expect(stream |> Enum.take(4))
+          |> to_eq Enum.map 1..4, fn (_) -> {data, []} end
+      end
     end
 
-    let :data do
-      payload = %{
-        "event" => "some::event",
-        "data"  => "some data",
-        "cid"   => "my-id"
-      }
-      {_, payload} = JSX.encode(payload)
-      payload
+    describe ".emit" do
+      before :each do
+        allow(Microbrew.Producer)
+          |> to_receive(publish: fn (_, _) -> nil end)
+
+        allow(Microbrew.Consumer)
+          |> to_receive(new: fn (_, _, _) -> {:ok, nil} end)
+
+        :ok
+      end
+
+      let :a_signal do
+        import Microbrew.Agent
+        Microbrew.Agent.new(
+          exchange:    "exchange",
+          queue:       "queue",
+          queue_error: "queue_error"
+        ) |> signal "some::event"
+      end
+
+      let :an_agent do
+        a_signal.agent
+      end
+
+      let :payload do
+        %{ :a_key => "some data" }
+      end
+
+      let :encoded_payload do
+        p = %Microbrew.Payload{
+          event: a_signal.event,
+          data: payload,
+          cid: a_signal.cid
+        }
+        {_, p} = JSX.encode p
+        p
+      end
+
+      it "publishes a signal with a given payload" do
+        a_signal |> emit payload
+
+        expect(Microbrew.Producer)
+          |> to_have_received :publish
+          |> with [an_agent.exchange, encoded_payload]
+      end
     end
 
-    before :each do
-      allow(AMQP.Basic)
-        |> to_receive(get: fn (_channel, _queue) ->
-          {:ok, data, []}
-        end)
-
-      :ok
-    end
-
-    it "returns a Stream of data" do
-      stream = a_signal |> stream
-
-      expect(stream |> Enum.take(4))
-        |> to_eq Enum.map 1..4, fn (_) -> {data, []} end
-    end
-  end
-
-  describe ".emit" do
-    before :each do
-      allow(Microbrew.Producer)
-        |> to_receive(publish: fn (_, _) -> nil end)
-
-      allow(Microbrew.Consumer)
-        |> to_receive(new: fn (_, _, _) -> {:ok, nil} end)
-
-      :ok
-    end
-
-    let :a_signal do
-      Microbrew.Agent.new(
-        exchange:    "exchange",
-        queue:       "queue",
-        queue_error: "queue_error"
-      ) |> signal "some::event"
-    end
-
-    let :an_agent do
-      a_signal.agent
-    end
-
-    let :payload do
-      %{ :a_key => "some data" }
-    end
-
-    let :encoded_payload do
-      p = %Microbrew.Payload{
-        event: a_signal.event,
-        data: payload,
-        cid: a_signal.cid
-      }
-      {_, p} = JSX.encode p
-      p
-    end
-
-    it "publishes a signal with a given payload" do
-      a_signal |> emit payload
-
-      expect(Microbrew.Producer)
-        |> to_have_received :publish
-        |> with [an_agent.exchange, encoded_payload]
-    end
-  end
-
-  describe ".stop" do
-    before :each do
-      allow(Microbrew.Consumer)
-        |> to_receive(close: fn (_) -> :ok end)
-
-      :ok
-    end
-
-    let :consumer do
-      %Microbrew.Consumer{channel: nil, queue: nil}
-    end
-
-    let :agent do
-      %Microbrew.Agent{
-        exchange:    "exchange",
-        queue:       "queue",
-        queue_error: "queue_error",
-        consumer:    consumer
-      }
-    end
-
-    it "closes the AMPQ channel" do
-      agent |> stop
-
-      expect(Microbrew.Consumer)
-        |> to_have_received :close
-        |> with agent.consumer
-    end
-
-    context "When the close operation returns :ok" do
+    describe ".stop" do
       before :each do
         allow(Microbrew.Consumer)
           |> to_receive(close: fn (_) -> :ok end)
@@ -343,23 +300,27 @@ defmodule AgentTest do
         :ok
       end
 
-      let :consumer do
-        %Microbrew.Consumer{channel: nil, queue: nil}
+      it "closes the AMPQ channel" do
+        sample |> stop
+
+        expect(Microbrew.Consumer)
+          |> to_have_received :close
+          |> with sample.consumer
       end
 
-      let :agent do
-        %Microbrew.Agent{
-          exchange:    "exchange",
-          queue:       "queue",
-          queue_error: "queue_error",
-          consumer:    consumer
-        }
-      end
+      context "When the close operation returns :ok" do
+        before :each do
+          allow(Microbrew.Consumer)
+            |> to_receive(close: fn (_) -> :ok end)
 
-      it "returns an Agent with no Consumer" do
-        a = agent |> stop
+          :ok
+        end
 
-        expect(a.consumer) |> to_be_nil
+        it "returns an Agent with no Consumer" do
+          a = sample |> stop
+
+          expect(a.consumer) |> to_be_nil
+        end
       end
     end
   end
